21fa69138c58e2deae8de5aeb05b99ec1e22dcaf
CereteuDumitru <dimacereteu@gmail.com>
2025-05-13 00:22:13 +0300

    Am adaugat home page-ul

--- Login/login.js
+++ Login/login.js
@@ -6,19 +6,22 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const password = document.getElementById('password').value;
     const passwordError = document.getElementById('passwordError');
 
-   
+    // Ascundem mesajele de eroare
     passwordError.style.display = 'none';
 
-
-  const passwordRegex = /^[A-Z].{7,}$/; 
+    // Validăm parola
+    const passwordRegex = /^[A-Z].{7,}$/; 
     if (!passwordRegex.test(password)) {
         passwordError.textContent = 'Password must start with an uppercase letter and be at least 8 characters long.';
         passwordError.style.display = 'block';
         return;
     }
 
-
+    // Afișăm un mesaj de bun venit (opțional)
     alert(`Welcome, ${firstName} ${lastName}! You are successfully logged in.`);
+
+    // Redirecționăm utilizatorul către pagina "homepage"
+    window.location.href = "../homepage/homepage.html"; 
 });
 
 function redirectToRegister() {
